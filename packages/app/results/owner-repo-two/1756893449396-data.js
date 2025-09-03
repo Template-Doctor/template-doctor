d26609b32b619ef8ec08c3fa44d56ef71e24b19a
anfibiacreativa <anfibiacreativa@users.noreply.github.com>
2025-09-03 09:57:30 +0000

    [TD:BOT]: update template analysis for https://github.com/owner/repo-two

---
+++ packages/app/results/owner-repo-two/1756893449396-data.js
@@ -0,0 +1,20 @@
+window.templateData = {
+  "compliance": {
+    "compliant": [
+      {
+        "id": "p1"
+      },
+      {
+        "id": "p2"
+      }
+    ],
+    "issues": [
+      {
+        "id": "i1"
+      }
+    ]
+  },
+  "repoUrl": "https://github.com/owner/repo-two",
+  "ruleSet": "dod",
+  "timestamp": "2025-09-03T09:57:15.905Z"
+};
